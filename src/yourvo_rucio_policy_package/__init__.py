4928734bbae0a28a6ddd11afc0940c5753b8d310
rdimaio <riccardodimaio11@gmail.com>
2025-11-03 13:54:25 +0000

    Make lfn2pfn a default algorithm; fix #5

--- src/yourvo_rucio_policy_package/__init__.py
+++ src/yourvo_rucio_policy_package/__init__.py
@@ -3,15 +3,11 @@ SUPPORTED_VERSION = [">=35.0.0"] # Specify the Rucio versions supported by this 
 def get_algorithms():
     from yourvo_rucio_policy_package.non_deterministic_pfn import YourVONonDeterministicPFNAlgorithm
     from yourvo_rucio_policy_package.scope import YourVOScopeExtractionAlgorithm
-    from yourvo_rucio_policy_package.lfn2pfn import YourVORSEDeterministicTranslation
     from yourvo_rucio_policy_package.pfn2lfn import YourVORSEDeterministicScopeTranslation
     from yourvo_rucio_policy_package.tape_metadata import YourVOTapeMetadataPlugin
     return { 
         'non_deterministic_pfn': {
             'yourvo_non_deterministic_pfn': YourVONonDeterministicPFNAlgorithm.construct_non_deterministic_pfn_yourvo
-            },
-        'lfn2pfn': {
-            'yourvo_lfn2pfn': YourVORSEDeterministicTranslation.lfn2pfn_yourvo
             },
         'pfn2lfn': {
             'yourvo_pfn2lfn': YourVORSEDeterministicScopeTranslation.pfn2lfn_yourvo
